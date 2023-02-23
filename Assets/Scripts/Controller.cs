72f966a9ba18f6d3abdbe85b9b32f90adda61002
Bydo <gamedevbydo@gmail.com>
2023-02-23 15:41:06 -0300

    Sistema de Seed
    falta fazer a checagem da seed de input do player

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Controller : MonoBehaviour
 {
@@ -44,16 +46,107 @@ public class Controller : MonoBehaviour
 
     #region Scene and Application Management
     
+        #region Scene Variables
+        //Quantas cenas jogaveis existem para a randomização, e um rastreador da quantidade de cenas jogaveis.
+        int playableScenes = 0, currentScene = 0;
+        //Lista das cenas randomizadas.
+        int[] sceneList;
+        // Seed atual
+        string seed;
+        #endregion
+
+        #region Scene References
+        public TextMeshProUGUI seedText, sceneCountText;
+        #endregion
+
     //Fechar o app
     public void ExitApp()
     {
         Application.Quit();
     }
-    //Será editado para que funcione com o sistema de ssed, mas é a versão temporária
-    public void LoadScene(string scene)
+    // Carrega uma cena com id especificado.
+    public void LoadScene(int sceneId)
     {
-        SceneManager.LoadScene(scene);
+        SceneManager.LoadScene(sceneId);
     }
 
+    // Carrega o menu, e altera os trem q mudar
+    public void LoadMenu()
+    {
+        LoadScene(0);
+        currentScene = 0;
+        WriteCurrentSceneText();
+    }
+
+    // Carrega a proxima cena da lista ja seedada
+    public void LoadNextScene()
+    {
+        LoadScene(sceneList[currentScene]);
+        currentScene++;
+        WriteCurrentSceneText();
+    }
+    // Cria uma string baseada na quantidade de cenas jogaveis disponiveis e retorna ela
+    public void CreateSeed()
+    {   
+        List<char> seedList = new List<char>();
+        for(int i = 0; i <playableScenes; i++)
+        {
+            char id = ((char)Random.Range(97,97+playableScenes));
+            while(seedList.Contains(id))
+            {
+                id = ((char)Random.Range(97,97+playableScenes));
+            }
+            seedList.Add(id);
+        }
+        seed = string.Concat(seedList);
+        WriteSeedText();
+    }
+
+    // Le uma seed e preenche a lista de fases na ordem correta.
+    public void ConvertSeedIntoList()
+    {
+        if(seed==null) CreateSeed();
+        string seedName = seed;
+        sceneList = new int[playableScenes];
+        char[] seedSplit = seedName.ToCharArray();
+        for(int i = 0; i < seedName.Length; i++)
+        {
+            sceneList[i] = (((byte)seedSplit[i])-97)+1;
+        }
+    }
+
+        #region Teste
+    public void CheckSeedIntegrity(string writtenSeed)
+    {
+        if(writtenSeed.Length != playableScenes) {CreateSeed(); return;}
+    }
+
+    public void InputSeed(string s)
+    {
+        seed = s.ToLower();
+        WriteSeedText();
+    }
+
+    public void WriteSeedText()
+    {
+        seedText.text = "Seed: "+seed;
+    }
+
+    public void WriteCurrentSceneText()
+    {
+        sceneCountText.text = "Cena:" +currentScene.ToString();
+    }
     #endregion
+
+    void BasicSetup() // Coisas para acontecerem no inicio do jogo.
+    {
+        playableScenes = SceneManager.sceneCountInBuildSettings - 1; // Alterar o valor baseado em quantas cenas não jogáveis existem
+        currentScene = 0;
+        seed = null;
+    }
+    #endregion
+    void Start()
+    {
+        BasicSetup();
+    }
 }
