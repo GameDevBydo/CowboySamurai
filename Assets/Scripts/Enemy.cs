44ffc078e9dc157b4ec40288545969688dd9ac56
zoiacode <126419420+zoiacode@users.noreply.github.com>
2023-03-01 09:16:36 -0300

    Update Enemy.cs
    Initial movement for enemy.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject player;
+    public float speed = 5.0f;
+
+    private float distance;
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        distance = Vector2.Distance(transform.position, player.transform.position);
+        Vector2 direction = player.transform.position - transform.position;
+
+        transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
+        if(distance <= 2)
+        {
+            speed = 0;
+            
+        }      
     }
 }
