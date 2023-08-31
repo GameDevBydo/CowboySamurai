b2bd6c3f4627b73f356a38501072a4d565d05492
Zoia <guilhermealmeidazuim@gmail.com>
2023-08-31 12:57:17 -0300

    Update Boss (Dash)
    Added the state machine for further use in animations + added boss dash

--- Assets/Scripts/Boss.cs
+++ Assets/Scripts/Boss.cs
@@ -1,16 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
-
 public class Boss : MonoBehaviour 
 {
     #region variables
+    public CharacterController controller;
+    private Animator anim;
 
     float baseLife = 250;
     float rageLife = 150;
     float normalLife = 100;
     private bool rageMode = false;
     private bool canAttack = false;
+    private bool canDash = false;
+    private bool getHit = false;
 
     public GameObject bossPrefab;
     public GameObject player;
@@ -25,6 +30,17 @@ public class Boss : MonoBehaviour
     #region Move
     //TODO: 
     //1: Dash(?)
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();
+    }
+    private void FixedUpdate() 
+    {
+        if (distance >= 10f && canDash)StartCoroutine(DashCD());
+        if (currentState == BossState.DASHING) controller.Move( transform.forward * Time.fixedDeltaTime * 10);
+
+    }
     void Follow()
     {
         distance = Vector2.Distance(transform.position, player.transform.position);
@@ -77,5 +93,71 @@ public class Boss : MonoBehaviour
     {
         return true;
     }
+    #endregion
+
+    #region StateMachine
+enum BossState
+    {
+        CINEMATIC,
+        STANDING,
+        WALKING,
+        ATTACKING,
+        HITSTUN,
+        DASHING
+    }
+
+    BossState currentState, pastState;
+    string animationName;
+    public void ChangeBossState(int id)
+    {
+        pastState = currentState;
+        if(currentState != pastState) return;
+
+        switch(id)
+        {
+            case 0:
+                currentState = BossState.CINEMATIC;
+            break;
+            case 1:
+                currentState = BossState.STANDING;
+            break;
+            case 2:
+                currentState = BossState.WALKING;
+            break;
+            case 3:
+                currentState = BossState.ATTACKING;
+            break;
+            case 4:
+                currentState = BossState.HITSTUN;
+            break;
+            case 5:
+                currentState = BossState.DASHING;
+            break;
+            default:
+                //Debug.Log("Change to state 1");
+                currentState = BossState.STANDING;
+            break;
+        }
+        anim.Play(animationName);
+        
+    }
+    #endregion
+    
+    #region IEnumerator
+    IEnumerator DashCD()
+        {
+            canDash = false;
+            getHit = false;
+            //rend.material = dashMat;
+            ChangeBossState(5);
+            gameObject.layer = LayerMask.NameToLayer("Dash");
+            yield return new WaitForSeconds(0.35f);
+            ChangeBossState(1);
+            gameObject.layer = LayerMask.NameToLayer("Boss");
+            getHit = true;
+            canDash = true;
+            //rend.material = baseMat;
+        }
+    
     #endregion
 }
