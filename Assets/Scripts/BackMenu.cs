493ea5aa4d08dad3f6decddd8ef8d65c585afb2c
Luis Henrique <luiscostasa@hotmail.com>
2023-11-22 10:51:04 -0300

    creditos ui

--- Assets/Scripts/BackMenu.cs
+++ Assets/Scripts/BackMenu.cs
@@ -2,10 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class BackMenu : MonoBehaviour
 {
+    public GameObject eventSystem, firstObj;
+    private void Start() {
+        eventSystem = GameObject.FindGameObjectWithTag("EV");
+        eventSystem.GetComponent<EventSystem>().firstSelectedGameObject = firstObj;
+    }
     public void Menu(){
         SceneManager.LoadScene("Menu");
     }
+
 }
