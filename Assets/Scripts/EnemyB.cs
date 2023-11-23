e617822981c7816ef864cbece2ca56a83a1f0fef
Luis Henrique Costa Silva Araujo <1117946@sga.pucminas.br>
2023-11-23 01:39:43 -0300

    hitbox inimigo porrete

--- Assets/Scripts/EnemyB.cs
+++ Assets/Scripts/EnemyB.cs
@@ -221,7 +221,6 @@ public class EnemyB : MonoBehaviour
     void CheckDeath(){
         if(hp <= 0)
         {
-            anim.Play("Death");
             Player.instance.exp += expDropped;
             Controller.instance.enemiesDefeated++;
             Controller.instance.CheckClearCondition();
@@ -232,7 +231,6 @@ public class EnemyB : MonoBehaviour
                 EnemyController.instance.rightEnemies.Remove(gameObject);
             Instantiate(deathPS, transform.position + Vector3.up, deathPS.transform.rotation);
             Instantiate(Player.instance.prefabCoin, new Vector3(transform.position.x,2f,transform.position.z), Quaternion.Euler(90f,0,0));
-            
             Destroy(gameObject);
         }
     }
